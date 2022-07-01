95b9b4cfeb2a043f22955412f479b61758aadc6a
Easonluoyuchen <36872955+Easonluoyuchen@users.noreply.github.com>
2022-07-01 23:42:17 +0800

    Update gridtrading.py

--- gridtrading.py
+++ gridtrading.py
@@ -1,6 +1,8 @@
 import time
 from GridTrader import FtxTrader
+#(market, top of the grid, bottom of the grid, grid number, qty)
 trader = FtxTrader("ETH-PERP", 3500, 2700, 15, 0.006)
 while True:
     trader.start()
-    time.sleep(30)
+    #Loop interval
+    time.sleep(10)
